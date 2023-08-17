a0a724432c441175d018479a5f23bb34caf70b1e
Sunntle <taikute012532@gmail.com>
2023-08-17 11:28:41 +0700

    Add folder admin

---
+++ admin/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className={"bg-slate-600 w-full h-full"}>
+            Hello World
+        </div>
+    );
+};
+
+export default App;
