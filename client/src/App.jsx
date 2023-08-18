67b51834f9a143971023d60214b80febd858a93c
huynhphu <phuhk2908@gmail.com>
2023-08-18 13:07:32 +0700

    Order Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import FoodCard from "./components/FoodCard.jsx";
-import OrderFood from "./pages/OrderFood/OrderFood.jsx";
 
 const App = () => {
     return (
-        <div className={"w-12/12 h-full"}>
-            <OrderFood/>
+        <div className={"bg-slate-600 w-full h-full"}>
+            Hello World
         </div>
     );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,11 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        'primary': '#FC8019'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
