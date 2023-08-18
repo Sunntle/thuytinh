686be3cb1a674d50ef631c6ab962020f16ba22c5
huynhphu <phuhk2908@gmail.com>
2023-08-18 15:03:21 +0700

    wip: update code order page

--- client/src/pages/OrderPage/OrderFood.jsx
+++ client/src/pages/OrderPage/OrderFood.jsx
@@ -4,81 +4,110 @@ import { HiMenu, HiOutlineClipboardCheck } from "react-icons/hi";
 import ButtonComponent from "../../components/ButtonComponent.jsx";
 import FoodCard from "../../components/FoodCard.jsx";
 import SidebarMobile from "./SidebarMobile.jsx";
+import SidebarTablet from "./SidebarTablet.jsx";
 
 const OrderFood = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    return (
-        <div className="w-12/12 h-screen  text-slate-800">
-            <div className="sm:hidden space-y-5 p-4">
-                <div className="flex justify-between items-center">
-                    <button>
-                        <FiChevronLeft className="w-7 h-7" />
-                    </button>
-                    <span className="text-2xl font-bold uppercase">Chọn món</span>
-                    <button>
-                        <HiOutlineClipboardCheck className="w-7 h-7" />
-                    </button>
-                </div>
-                <div className="w-full h-20 drop-shadow-md">
-                    <img
-                        src="https://images.unsplash.com/photo-1538333581680-29dd4752ddf2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80"
-                        alt=""
-                        className="w-full h-full aspect-video object-cover"
-                    />
-                </div>
-                <div className="flex justify-between items-center">
-                    <ButtonComponent
-                        onClick={() => setIsSidebarOpen(true)}
-                        className={"py-1 px-2 rounded flex items-center gap-1"}
-                        type={"outline"}
-                        size={"md"}
-                    >
-                        <HiMenu className="w-4 h-4" />
-                        <span>Menu</span>
-                    </ButtonComponent>
-                </div>
-                <div className="pt-3 space-y-5">
-                    {/*Món mới nhất*/}
-                    <div className="flex flex-col space-y-5">
-            <span className="text-xl font-bold uppercase">
-              Món bán chạy nhất
-            </span>
-                        <div className="grid grid-cols-2 gap-4">
-                            <FoodCard />
-                            <FoodCard />
-                            <FoodCard />
-                            <FoodCard />
-                        </div>
-                    </div>
-                    {/*Món bán chạy nhất*/}
-                    <div className="flex flex-col space-y-5">
-            <span className="text-xl font-bold uppercase">
-              Món bán chạy nhất
-            </span>
-                        <div className="grid grid-cols-2 gap-4">
-                            <FoodCard />
-                            <FoodCard />
-                            <FoodCard />
-                            <FoodCard />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="sm:hidden">
-                <SidebarMobile isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
-            </div>
-            <ButtonComponent
-                disable={true}
-                className={
-                    "fixed sm:hidden rounded-lg py-3 h-12 bottom-2 left-1/2 transform -translate-x-1/2 w-11/12"
-                }
-                type={"primary"}
-                size={"md"}
-                children={"Tiếp tục"}
+  return (
+    <div className="w-12/12 h-screen  text-slate-800">
+      <div className="sm:hidden">
+        <div className="space-y-5 p-4">
+          <div className="flex justify-between items-center">
+            <button>
+              <FiChevronLeft className="w-7 h-7" />
+            </button>
+            <span className="text-2xl font-bold uppercase">Chọn món</span>
+            <button>
+              <HiOutlineClipboardCheck className="w-7 h-7" />
+            </button>
+          </div>
+          <div className="w-full h-20 drop-shadow-md">
+            <img
+              src="https://images.unsplash.com/photo-1538333581680-29dd4752ddf2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80"
+              alt=""
+              className="w-full h-full aspect-video object-cover"
             />
+          </div>
+          <div className="flex justify-between items-center">
+            <ButtonComponent
+              onClick={() => setIsSidebarOpen(true)}
+              className={"py-1 px-2 rounded flex items-center gap-1"}
+              type={"outline"}
+              size={"md"}
+            >
+              <HiMenu className="w-4 h-4" />
+              <span>Menu</span>
+            </ButtonComponent>
+          </div>
+          <div className="pt-3 space-y-5">
+            {/*Món mới nhất*/}
+            <div className="flex flex-col space-y-5">
+              <span className="text-xl font-bold uppercase">
+                Món bán chạy nhất
+              </span>
+              <div className="grid grid-cols-2 gap-4">
+                <FoodCard />
+                <FoodCard />
+                <FoodCard />
+                <FoodCard />
+              </div>
+            </div>
+            {/*Món bán chạy nhất*/}
+            <div className="flex flex-col space-y-5">
+              <span className="text-xl font-bold uppercase">
+                Món bán chạy nhất
+              </span>
+              <div className="grid grid-cols-2 gap-4">
+                <FoodCard />
+                <FoodCard />
+                <FoodCard />
+                <FoodCard />
+              </div>
+            </div>
+          </div>
         </div>
-    );
+        <SidebarMobile
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <ButtonComponent
+          disable={true}
+          className={
+            "fixed sm:hidden rounded-lg py-3 h-12 bottom-2 left-1/2 transform -translate-x-1/2 w-11/12"
+          }
+          type={"primary"}
+          size={"md"}
+          children={"Tiếp tục"}
+        />
+      </div>
+      {/* Layout Tablet */}
+      <div className="grid grid-cols-12 gap-4">
+        {/*Sidebar Left Content*/}
+        <div className="col-span-4 lg:col-span-3 border-r">
+          <SidebarTablet />
+        </div>
+        {/* Main Content */}
+        <div className="col-span-7 lg:col-span-8 border-x">
+          <div className="p-4 w-full max-h-screen overflow-y-auto">
+            {/* Filter */}
+            <div className="w-full h-16 bg-slate-200 mb-8"></div>
+            {/* List Product */}
+            <div className="flex flex-wrap items-center justify-between gap-4">
+              <FoodCard/>
+              <FoodCard/>
+              <FoodCard/>
+              <FoodCard/>
+              <FoodCard/>
+              <FoodCard/>
+            </div>
+          </div>
+        </div>
+        {/* Sidebar Right Content */}
+        <div className="col-span-1"></div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderFood;
--- client/src/pages/OrderPage/SubMenu.jsx
+++ client/src/pages/OrderPage/SubMenu.jsx
@@ -1,28 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiChevronRight } from "react-icons/hi2";
+import { motion } from "framer-motion";
 
-const SubMenu = ({ data }) => {
-    return (
-        <div className="flex flex-col space-y-5 w-full px-1 mt-10">
-            {/* Menu */}
-            <div>
-                <div className="px-3 py-3 flex justify-between items-center bg-primary bg-opacity-[15%] text-primary rounded-lg">
-                    <span className="text-2xl font-medium">{data.name}</span>
-                    <HiChevronRight className="w-6 h-6" />
-                </div>
-                {/* Submenu */}
-                <div className="flex flex-col space-y-5 ml-10 mt-3 ">
-                    {data?.menus?.map((menu,index) => (
-                        <div key={index} className="relative">
-              <span className="text-xl font-medium after:absolute after:-bottom-1 after:left-0 after:w-full after:border-b active:text-primary active:after:border-primary">
-                {menu}
-              </span>
-                        </div>
-                    ))}
-                </div>
-            </div>
+const SubMenu = ({ data, index }) => {
+  const [activeKey, setActiveKey] = useState(null);
+
+  const initialMotion = {
+    open: {
+      x: 0,
+      height: "auto",
+      transition: {
+        easeInOut: 1,
+      },
+    },
+    closed: {
+      x: -450,
+      height: 0,
+      transition: {
+        easeInOut: 1,
+      },
+    },
+  };
+
+  return (
+    <div className="flex flex-col space-y-3 w-full px-1 mb-5">
+      {/* Menu */}
+      <div>
+        <div
+          onClick={() => setActiveKey(activeKey === index ? null : index)}
+          className={`px-3 py-3 mb-3 flex justify-between items-center rounded-lg ${
+            activeKey === index
+              ? "active:bg-primary active:bg-opacity-[15%] active:text-primary"
+              : ""
+          }`}
+        >
+          <span className="text-2xl font-medium">{data.name}</span>
+          <HiChevronRight className={`w-6 h-6 transition-transform transform duration-300 ${activeKey === index ? 'rotate-90' : ''}`} />
         </div>
-    );
+        {/* Submenu */}
+        <motion.div
+          variants={initialMotion}
+          animate={activeKey === index ? "open" : "closed"}
+          transition={{ duration: 0.3 }}
+        >
+          <div className="flex flex-col space-y-5 ml-10">
+            {data?.menus?.map((menu, index) => (
+              <div key={index} className="relative">
+                <span className="text-xl font-medium after:absolute after:-bottom-1 after:left-0 after:w-full after:border-b active:text-primary active:after:border-primary">
+                  {menu}
+                </span>
+              </div>
+            ))}
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default SubMenu;
