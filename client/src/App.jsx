9d04ab158a7c6a7fc9fa45c911d75c60e57970c4
lemyduynnn <duyenle.843703@gmail.com>
2023-08-19 23:25:12 +0700

    update sidebar choosetablepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
-
-import React from 'react';
+import React from "react";
 import OrderFood from "./pages/OrderPage/OrderFood.jsx";
 import OrderListMobile from "./pages/OrderPage/OrderListMobile.jsx";
+import ChooseTablePage from "./pages/ChooseTablePage";
 
 const App = () => {
-    return (
-        <div className={"w-12/12 h-full"}>
-            <OrderFood />
-        </div>
-    );
+  return (
+    <div className={"w-12/12 h-full"}>
+      <ChooseTablePage />
+    </div>
+  );
 };
 
 export default App;
