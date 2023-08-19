219f792dbef65f8929fc27a90333305eafe3a44a
phuhk2908 <phuhk2908@gmail.com>
2023-08-19 13:34:42 +0700

    wip

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import OrderFood from "./pages/OrderPage/OrderFood.jsx";
+import OrderListMobile from "./pages/OrderPage/OrderListMobile.jsx";
 
 const App = () => {
     return (
         <div className={"w-12/12 h-full"}>
-            <OrderFood/>
+            <OrderFood />
         </div>
     );
 };
