810dfd74fe4a857f98d356bd251d1ba1043029e8
Sunntle <taikute012532@gmail.com>
2023-09-11 16:36:47 +0700

    Remove edit recipes in product

--- server/controller/recipeController.js
+++ server/controller/recipeController.js
@@ -70,32 +70,10 @@ exports.updateRecipe = asyncHandler(async (req, res) => {
   };
   if (removedItems?.length > 0) {
     await Recipes.destroy({ where: { id: removedItems } });
-};
-exports.updateRecipeById = async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const response = await Recipes.update(req.body, {
-      where: { id_product: _id },
-    });
-    res.status(200).json("Cập nhật công thức thành công !");
-  } catch (err) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
-exports.removeRecipe = async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const response = await Recipes.destroy({
-      where: { id: _id },
-    });
-    res.status(200).json("Xóa công thức thành công");
-  } catch (err) {
-    res.status(500).json({ error: "Internal server error" });
   }
   res.status(200).json("Cập nhật công thức thành công !");
-
-})
-exports.removeRecipeByProductId = asyncHandler(async (req, res) => {
+});
+exports.removeRecipe = asyncHandler(async (req, res) => {
   await Recipes.destroy({
     where: { id_product: req.params.id }
   }).catch((err) => {
