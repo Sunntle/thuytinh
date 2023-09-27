af3a570d6eb438659e173d4c2bd55af52c918c05
phuhuynh <phuhk2908@gmail.com>
2023-09-27 15:01:42 +0700

    wip: fix layout enter-name and create product detail on create product detail modal on mobile

--- client/src/pages/EnterName/EnterName.jsx
+++ client/src/pages/EnterName/EnterName.jsx
@@ -19,18 +19,20 @@ const EnterName = () => {
     }
 
     return (
-        <div className="h-screen w-screen flex justify-center items-center">
-            <div className="pb-24 lg:py-24 flex flex-col items-center space-y-3">
-                <div className="w-6/12 m-auto">
+        <div className="h-screen w-screen flex items-center">
+            <div className="pb-24 lg:pb-0 lg:px-36 lg:py-24 flex flex-col lg:flex-row justify-center items-center space-y-3">
+                <div className="w-6/12 lg:w-6/12 m-auto">
                     <img
                         className="w-full h-full"
                         src={serviceImg}
                         alt=""
                     />
                 </div>
-                <span className="font-medium text-sm w-9/12 lg:text-xl text-center">Vui lòng nhập tên để chúng em tiện xưng hô và phục vụ tốt nhất</span>
-                <input onChange={handleChangeName} value={customerName} type="text" className="w-9/12 h-12 border rounded-lg pl-3" placeholder="Nhập tên"/>
-                <button onClick={handleSubmitName} className="w-9/12 h-12 bg-primary text-white active:bg-opacity-80 rounded-lg text-lg font-medium">Tiếp tục</button>
+               <div className="flex flex-col items-center space-y-3 lg:space-y-6">
+                   <span className="font-medium text-sm w-9/12 lg:text-xl text-center">Vui lòng nhập tên để chúng em tiện xưng hô và phục vụ tốt nhất</span>
+                   <input onChange={handleChangeName} value={customerName} type="text" className="w-9/12 h-12 border rounded-lg pl-3" placeholder="Nhập tên"/>
+                   <button onClick={handleSubmitName} className="w-9/12 h-12 bg-primary text-white active:bg-opacity-80 rounded-lg text-lg font-medium">Tiếp tục</button>
+               </div>
             </div>
         </div>
     );
