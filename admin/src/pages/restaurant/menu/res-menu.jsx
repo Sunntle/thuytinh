91c1e2069d52ee29249b7c09488fb00dfa202e34
PhamHanHi <huyphps20855@fpt.edu.vn>
2023-10-30 19:07:07 +0700

    fix layout menu

--- admin/src/pages/restaurant/menu/res-menu.jsx
+++ admin/src/pages/restaurant/menu/res-menu.jsx
@@ -34,7 +34,7 @@ const ResMenu = () => {
         setPage(e)
     }
     return (
-        <div className='w-full p-10'>
+        <div className='w-full px-5 py-10'>
             <FloatButton.BackTop />
             <Row gutter={[16, 16]}>
                 <Col xs={24} lg={16}>
@@ -48,7 +48,7 @@ const ResMenu = () => {
                             {categories?.map((category, index) => (
                                 <SwiperSlide key={index}>
                                     <div className="mx-10">
-                                        <Button className='border-none text-main bg-orange-100 w-full max-w-full overflow-hidden' onClick={() => setSelectedCategory(category)}>
+                                        <Button className='border-none text-main bg-orange-100 w-full max-w-full lg:w-full lg:max-w-full overflow-hidden' onClick={() => setSelectedCategory(category)}>
                                             {category.name_category}
                                         </Button>
                                     </div>
@@ -62,14 +62,14 @@ const ResMenu = () => {
                                 <Divider><span className='font-medium text-main text-lg'>{selectedCategory.name_category}</span></Divider>
                             </div>
                         )}
-                        <Row gutter={[16, 16]}>
+                        <Row gutter={[32, 24, 16, 8 ]}>
                             {filteredProducts?.map((product, index) => (
-                                <Col lg={8} md={12} sm={12} xs={12} className='rounded-lg' key={index}>
+                                <Col lg={8} md={8} sm={12} xs={12} className='rounded-lg' key={index}>
                                     {product.discount > 0 ? (
                                         <Badge.Ribbon text={`${product.discount}%`} color='red'>
-                                            <div className='shadow-xl border-solid border border-gray-300 rounded-lg min-h-[250px] w-auto' >
+                                            <div className='shadow-xl border-solid border border-gray-300 rounded-lg min-h-[230px] w-auto' >
                                                 <img className='h-full w-full rounded-t-lg' src={product?.ImageProducts[0]?.url} />
-                                                <div className='p-4 flex flex-col'>
+                                                <div className='p-2 flex flex-col'>
                                                     <div className='font-medium'>{product.name_product}</div>
                                                     <div className='text-xs mt-2 text-slate-500'>Số lượng : {product.amount}</div>
                                                     <div className='flex justify-between items-center'>
@@ -77,20 +77,20 @@ const ResMenu = () => {
                                                             <p className=' font-medium text-main text-lg mr-1'> {(formatGia(product.price - (product.price * product.discount / 100)))}</p>
                                                             <p className=' font-medium text-slate-300 line-through text-xs '> {(formatGia(product.price))}</p>
                                                         </div>
-                                                        <PlusOutlined onClick={() => dispatch(AddCart(product))} size={30} className='p-2 bg-main rounded-full text-white' />
+                                                        <PlusOutlined onClick={() => dispatch(AddCart(product))} size={30} className='p-1 bg-main rounded-full text-white' />
                                                     </div>
                                                 </div>
                                             </div>
                                         </Badge.Ribbon>
                                     ) : (
-                                        <div className='shadow-xl border-solid border border-gray-300 rounded-lg h-auto w-auto'>
+                                        <div className='shadow-xl border-solid border border-gray-300 rounded-lg min-h-[230px] h-auto w-auto'>
                                             <img className='h-full w-full rounded-t-lg' src={product?.ImageProducts[0]?.url} />
-                                            <div className='p-4'>
-                                                <div className='font-medium h-5'>{product.name_product}</div>
-                                                <div className='text-xs text-slate-500'>Số lượng : {product.amount}</div>
+                                            <div className='p-2 flex flex-col'>
+                                                <div className='font-medium'>{product.name_product}</div>
+                                                <div className='text-xs text-slate-500 mt-2'>Số lượng : {product.amount}</div>
                                                 <div className='flex justify-between items-center'>
                                                     <p className=' font-medium text-main text-lg mt-1'> {(formatGia(product.price))}</p>
-                                                    <PlusOutlined onClick={() => dispatch(AddCart(product))} size={30} className='p-2 bg-main rounded-full text-white' />
+                                                    <PlusOutlined onClick={() => dispatch(AddCart(product))} size={30} className='p-1 bg-main rounded-full text-white' />
                                                 </div>
                                             </div>
                                         </div>)}
