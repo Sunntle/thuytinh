824be7e047f9863cadd034452b75a756985aa281
kimphu2908 <phuhk2908@gmail.com>
2023-12-13 16:51:21 +0700

    wip: fix ui

--- client/src/components/CallStaff/CallStaff.jsx
+++ client/src/components/CallStaff/CallStaff.jsx
@@ -7,7 +7,7 @@ import { useSelector } from "react-redux";
 const CallStaff = () => {
   const [isDisabled, setIsDisabled] = useState(false);
   const [messageApi, contextHolder] = message.useMessage();
-  const {tables} = useSelector((state) => state.customerName);
+  const { tables } = useSelector((state) => state.customerName);
 
   const handleCallStaff = async () => {
     if (isDisabled) return;
@@ -38,11 +38,16 @@ const CallStaff = () => {
       <button
         disabled={isDisabled}
         onClick={handleCallStaff}
-        className={`${isDisabled ? 'bg-transparent' :'bg-white'} cursor-pointer fixed p-2 right-5 lg:right-20 bottom-24 w-auto h-auto border-2 active:bg-primary active:text-white transition-all border-primary rounded-full flex justify-center items-center `}
-
+        className={`${
+          isDisabled ? "hidden" : "bg-white"
+        } cursor-pointer fixed p-2 right-5 lg:right-20 bottom-24 w-auto h-auto border-2 active:bg-primary active:text-white transition-all border-primary rounded-full flex justify-center items-center z-40`}
       >
-        <span className="animate-ping absolute inline-flex h-10 w-10 rounded-full bg-primary opacity-30"></span>
-        <HiOutlineBellAlert className="w-6 h-6 lg:w-8 lg:h-8 text-primary animate-swing active:text-white transition-colors" />
+        <span
+          className={`absolute inline-flex h-10 w-10 rounded-full bg-primary opacity-30 animate-ping`}
+        ></span>
+        <HiOutlineBellAlert
+          className={`w-6 h-6 lg:w-8 lg:h-8 text-primary animate-swing`}
+        />
       </button>
     </>
   );
